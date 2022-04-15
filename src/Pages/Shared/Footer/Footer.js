055fa6c7d755ef1c297b1393ce392fa1e432ce96
SEC-MASUM <masum.jess42@gmail.com>
2022-04-16 00:04:20 +0600

    Footer Componet design

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   const today = new Date();
   const year = today.getFullYear();
   return (
-    <div>
+    <div className="flex justify-center">
       <p>Copyright &copy; {year}</p>
     </div>
   );
