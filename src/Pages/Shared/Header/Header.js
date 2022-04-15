a5f42c6f4eedc1489ab46ab9d1cda3ccb8d79e3d
SEC-MASUM <masum.jess42@gmail.com>
2022-04-16 00:02:08 +0600

    Navbar redesign

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,38 +2,35 @@ import React from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import logo from "../../../Assets/images/logo/logo2.png";
 import { FiShoppingCart } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <Navbar
-      collapseOnSelect
-      expand="lg"
-      sticky="top"
-      className="text-dark py-3"
-      bg="white"
 
-    >
-      <Container>
-        <Navbar.Brand href="#home">
-          <img height={"32px"} src={logo} alt="" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#cart">
-              <FiShoppingCart></FiShoppingCart>
-            </Nav.Link>
-            <Nav.Link href="#login">Login</Nav.Link>
-            <Nav.Link
-              href="#signUp"
-              className="bg-red text-white rounded-pill px-4"
-            >
-              Sign up
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+  return (
+    <nav className="bg-white px-2 py-3  ">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <Link to="/">
+          <img src={logo} className="h-10" alt="Red Onion Logo" />
+        </Link>
+
+        <ul className="flex space-x-8 text-sm font-medium items-center">
+          <Link to="/cart" className="text-gray-700 hover:text-rose-600 ">
+            <FiShoppingCart></FiShoppingCart>
+          </Link>
+
+          <Link to="/login" className="text-gray-700 hover:text-rose-600 ">
+            Login
+          </Link>
+
+          <Link
+            to="/signup"
+            className="text-white bg-rose-600 px-4 py-1.5 rounded-full"
+          >
+            Sign up
+          </Link>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
