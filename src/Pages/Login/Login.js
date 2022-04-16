61a5cd6d243d4ec5367fba566efb99f700274232
SEC-MASUM <masum.jess42@gmail.com>
2022-04-16 16:29:53 +0600

    create user and login with email and password by using useSignInWithEmailAndPassword() and createUserWithEmailAndPassword() hooks

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,40 +1,69 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../Assets/images/logo/logo3.png";
 import { BsFacebook, BsGithub } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
+import auth from "../../Firebase/firebase.init";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+    signInWithEmailAndPassword(email, password);
+  };
+
+  if (user) {
+    navigate("/");
+  }
+  if (error) {
+    console.error(error);
+  }
+
   return (
     <div className="container mx-auto">
-      <div class="text-center  w-[500px] pb-7 mx-auto">
+      <div className="text-center  w-[500px] pb-7 mx-auto">
         <div>
           <div className="my-4">
             <img src={logo} alt="" className="h-20 mx-auto" />
           </div>
-          <h2 class="text-3xl font-bold text-rose-600 mb-5 capitalize">
+          <h2 className="text-3xl font-bold text-rose-600 mb-5 capitalize">
             Log In
           </h2>
-          <input
-            id="email"
-            class="focus:ring-gray-300  focus:ring-2 focus:bg-white w-4/5  bg-gray-100 border-0 shadow rounded-lg px-4 py-3 mb-5"
-            type="email"
-            placeholder="Email"
-            required
-          />
-          <input
-            id="password"
-            class="focus:ring-gray-300  focus:ring-2 focus:bg-white w-4/5  bg-gray-100 border-0 shadow rounded-lg px-4 py-3 mb-5"
-            type="password"
-            placeholder="Password"
-            required
-          />
-          <button
-            id="login-button"
-            class="w-4/5 font-medium text-white bg-rose-600 uppercase block shadow-gray-400 shadow-sm rounded-lg px-6 py-3 mb-3 mx-auto"
-          >
-            Log in
-          </button>
+          <form onSubmit={handleLogin}>
+            <input
+              id="email"
+              className="focus:ring-gray-300  focus:ring-2 focus:bg-white w-4/5  bg-gray-100 border-0 shadow rounded-lg px-4 py-3 mb-5"
+              type="email"
+              placeholder="Email"
+              required
+            />
+            <input
+              id="password"
+              className="focus:ring-gray-300  focus:ring-2 focus:bg-white w-4/5  bg-gray-100 border-0 shadow rounded-lg px-4 py-3 mb-5"
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <button
+              id="login-button"
+              type="submit"
+              className="w-4/5 font-medium text-white bg-rose-600 uppercase block shadow-gray-400 shadow-sm rounded-lg px-6 py-3 mb-3 mx-auto"
+            >
+              Log in
+            </button>
+          </form>
+          {error ? (
+            <p className="text-red-600 font-medium pb-4">{error.message}</p>
+          ) : (
+            ""
+          )}
           <p>
             New to Red Onion?{" "}
             <Link to="/signup">
@@ -45,7 +74,7 @@ const Login = () => {
         <div className="flex justify-center mt-3">
           <p className="flex w-3/5 text-center items-center ">
             <div className="w-full block h-1 bg-rose-600"></div>
-            <div class="block px-4">Or</div>
+            <div className="block px-4">Or</div>
             <div className="w-full block h-1 bg-rose-600"></div>
           </p>
         </div>
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,10 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useRef } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import logo from "../../Assets/images/logo/logo3.png";
 import { BsFacebook, BsGithub } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
+import auth from "../../Firebase/firebase.init";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const confirmPasswordRef = useRef("");
+
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const confirmPassword = confirmPasswordRef.current.value;
+    console.log(name, email, password, confirmPassword);
+    createUserWithEmailAndPassword(email, password);
+  };
+
+  if (user) {
+    navigate("/");
+  }
+
   return (
     <div className="container mx-auto">
       <div className="text-center  w-[500px] pb-7 mx-auto">
@@ -15,41 +41,52 @@ const SignUp = () => {
           <h2 className="text-3xl font-bold text-rose-600 mb-5 capitalize">
             Sign Up
           </h2>
+          <form onSubmit={handleSignUp}>
+            <input
+              id="name"
+              ref={nameRef}
+              className="focus:ring-gray-300  focus:ring-2 focus:bg-white w-4/5  bg-gray-100 border-0 shadow rounded-lg px-4 py-3 mb-5"
+              type="text"
+              placeholder="Name"
+            />
+            <input
+              id="email"
+              ref={emailRef}
+              className="focus:ring-gray-300  focus:ring-2 focus:bg-white w-4/5  bg-gray-100 border-0 shadow rounded-lg px-4 py-3 mb-5"
+              type="email"
+              placeholder="Email"
+              required
+            />
+            <input
+              id="password"
+              ref={passwordRef}
+              className="focus:ring-gray-300  focus:ring-2 focus:bg-white w-4/5  bg-gray-100 border-0 shadow  rounded-lg px-4 py-3 mb-5"
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <input
+              id="confirm-password"
+              ref={confirmPasswordRef}
+              className="focus:ring-gray-300  focus:ring-2 focus:bg-white  w-4/5  bg-gray-100 border-0 shadow  rounded-lg px-4 py-3 mb-5"
+              type="password"
+              placeholder="Confirm Password"
+              required
+            />
+            <button
+              id="signup-button"
+              type="submit"
+              className="w-4/5 font-medium text-white bg-rose-600 uppercase block  shadow-sm rounded-lg px-6 py-3 mb-3 mx-auto"
+            >
+              Sign Up
+            </button>
+            {error ? (
+              <p className="text-red-600 font-medium pb-4">{error.message}</p>
+            ) : (
+              ""
+            )}
+          </form>
 
-          <input
-            id="name"
-            className="focus:ring-gray-300  focus:ring-2 focus:bg-white w-4/5  bg-gray-100 border-0 shadow rounded-lg px-4 py-3 mb-5"
-            type="text"
-            placeholder="Name"
-            required
-          />
-          <input
-            id="email"
-            className="focus:ring-gray-300  focus:ring-2 focus:bg-white w-4/5  bg-gray-100 border-0 shadow rounded-lg px-4 py-3 mb-5"
-            type="email"
-            placeholder="Email"
-            required
-          />
-          <input
-            id="password"
-            className="focus:ring-gray-300  focus:ring-2 focus:bg-white w-4/5  bg-gray-100 border-0 shadow  rounded-lg px-4 py-3 mb-5"
-            type="password"
-            placeholder="Password"
-            required
-          />
-          <input
-            id="confirm-password"
-            className="focus:ring-gray-300  focus:ring-2 focus:bg-white  w-4/5  bg-gray-100 border-0 shadow  rounded-lg px-4 py-3 mb-5"
-            type="password"
-            placeholder="Confirm Password"
-            required
-          />
-          <button
-            id="login-button"
-            className="w-4/5 font-medium text-white bg-rose-600 uppercase block  shadow-sm rounded-lg px-6 py-3 mb-3 mx-auto"
-          >
-            Sign Up
-          </button>
           <p>
             Already Have An Account?{" "}
             <Link to="/login">
@@ -58,11 +95,11 @@ const SignUp = () => {
           </p>
         </div>
         <div className="flex justify-center mt-3">
-          <p className="flex w-3/5 text-center items-center ">
+          <div className="flex w-3/5 text-center items-center ">
             <div className="w-full block h-1 bg-rose-600"></div>
             <div className="block px-4">Or</div>
             <div className="w-full block h-1 bg-rose-600"></div>
-          </p>
+          </div>
         </div>
         <div className="flex justify-center items-center p-5 space-x-4">
           <button id="facebook-login" className="text-2xl  text-blue-600">
