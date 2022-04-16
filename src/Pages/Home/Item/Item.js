7e83521d79653a23f261bf1a23915b358c3ba06f
SEC-MASUM <masum.jess42@gmail.com>
2022-04-16 07:38:06 +0600

    add Add to Cart button in Item card component

--- src/Pages/Home/Item/Item.js
+++ src/Pages/Home/Item/Item.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { FiShoppingCart } from "react-icons/fi";
 
 const Item = ({ item }) => {
   const { img, title, desc, price } = item;
   return (
     <div>
-      <div className="w-72 flex flex-col items-center rounded-md hover:shadow-lg p-8 mx-auto">
+      <div className="w-72  flex flex-col items-center rounded-md hover:shadow-lg p-8 mx-auto ">
         <img src={img} alt="" className="h-50" />
         <h3 className="font-medium text-base pt-4">{title}</h3>
         <p>
@@ -14,6 +15,11 @@ const Item = ({ item }) => {
           <span>$</span>
           {price}
         </h2>
+        <button className="space-x-2 flex items-center bg-rose-600  text-white px-4 py-2 rounded-md cursor-pointer ">
+          <FiShoppingCart className="inline-block"></FiShoppingCart>
+
+          <span>Add to Cart</span>
+        </button>
       </div>
     </div>
   );
