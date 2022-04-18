f35a8f86738836e002a65a349281aac23dac6ddb
SEC-MASUM <masum.jess42@gmail.com>
2022-04-18 07:37:34 +0600

    refactor : rename class to className

--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -5,10 +5,10 @@ const Loading = () => {
     <div className="w-full h-[400px] flex justify-center items-center">
       <button
         type="button"
-        class="bg-indigo-500 pr-10 py-2 text-white font-bold text-2xl flex justify-between items-center rounded-xl"
+        className="bg-indigo-500 pr-10 py-2 text-white font-bold text-2xl flex justify-between items-center rounded-xl"
         disabled
       >
-        <svg class="animate-spin h-5 w-5 mr-5" viewBox="0 0 24 24"></svg>
+        <svg className="animate-spin h-5 w-5 mr-5" viewBox="0 0 24 24"></svg>
         Processing...
       </button>
     </div>
